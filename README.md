3e2a96f07bb79d700402a73745c78abf2e25ec94
talamortis <talamortis@gmail.com>
2017-11-30 18:00:41 +0000

    Module Import

---
+++ README.md
@@ -0,0 +1,36 @@
+# Who Logged
+
+This is a module for [AzerothCore](http://www.azerothcore.org)
+
+Current features:
+
+-**This Module outputs to the console when a player logs into the world.
+
+Upcoming features:
+
+
+## Requirements
+
+Who logged Module currently requires:
+
+AzerothCore v1.0.1+
+
+## How to install
+
+###1) Simply place the module under the `modules` folder of your AzerothCore source folder.
+
+###2) Input the SQL file to the character database. (if any)
+
+###3) Re-run cmake and launch a clean build of AzerothCore
+
+**That's it.**
+
+### (Optional) Edit module configuration
+
+If you need to change the module configuration, go to your server configuration folder (e.g. **etc**), copy `who-logged.conf.dist` to `who-logged.conf` and edit it as you prefer.
+
+
+
+
+
+
---
+++ src/who_logged.cpp
@@ -0,0 +1,88 @@
+#include "Configuration/Config.h"
+#include "Player.h"
+#include "Creature.h"
+#include "AccountMgr.h"
+#include "ScriptMgr.h"
+#include "Define.h"
+#include "GossipDef.h"
+
+class WhoLoggedAnnounce : public PlayerScript
+{
+public:
+        WhoLoggedAnnounce() : PlayerScript("WhoLoggedAnnounce") { }
+
+
+    void OnLogin(Player* p)
+    {
+        std::string playerIP = p->GetSession()->GetRemoteAddress();
+        std::string playerName = p->GetName();
+        uint32 pAccountID = p->GetSession()->GetAccountId();
+        uint32 pLevel = p->getLevel();
+        std::string pClass;
+
+        if (!sConfigMgr->GetBoolDefault("PlayerAnnounce", true))
+        {
+            return;
+        }
+
+        switch (p->getClass())
+        {
+        case CLASS_WARLOCK:
+            pClass = "Warlock";
+            break;
+        case CLASS_WARRIOR:
+            pClass = "Warrior";
+            break;
+        case CLASS_MAGE:
+            pClass = "Mage";
+            break;
+        case CLASS_SHAMAN:
+            pClass = "Shaman";
+            break;
+        case CLASS_DEATH_KNIGHT:
+            pClass = "Death Knight";
+            break;
+        case CLASS_DRUID:
+            pClass = "Druid";
+            break;
+        case CLASS_HUNTER:
+            pClass = "Hunter";
+            break;
+        case CLASS_PALADIN:
+            pClass = "Paladin";
+            break;
+        case CLASS_ROGUE:
+            pClass = "Rogue";
+            break;
+        case CLASS_PRIEST:
+            pClass = "Priest";
+            break;
+        }
+
+        sLog->outString("Player '%s' has Logged In : Level '%u' : Class = '%s' : IP: '%s' : AccountID = '%u'", playerName.c_str(), pLevel, pClass.c_str(), playerIP.c_str(), pAccountID);
+    }
+};
+
+class who_logged_conf : public WorldScript
+{
+public:
+    who_logged_conf() : WorldScript("who-logged_conf") { }
+
+    void OnBeforeConfigLoad(bool reload) override
+    {
+        if (!reload) {
+            std::string cfg_file = "who-logged.conf";
+            std::string cfg_def_file = cfg_file + ".dist";
+
+            sConfigMgr->LoadMore(cfg_def_file.c_str());
+
+            sConfigMgr->LoadMore(cfg_file.c_str());
+        }
+    }
+};
+
+void AddWhoLoggedScripts()
+{
+    new WhoLoggedAnnounce();
+    new who_logged_conf();
+}
